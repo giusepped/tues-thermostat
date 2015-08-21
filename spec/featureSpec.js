70598ef3137566628e83ad4983dcfd85d94ce4ea
giuseppe de santis <giusdesan@gmail.com>
2015-08-21 11:38:06 +0100

    tested jquery interface - not API call yet - all green

---
+++ spec/featureSpec.js
@@ -0,0 +1,66 @@
+describe('Thermostat feature tests', function(){
+
+  beforeEach(function(){
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+
+  it('should be 20 degrees by default', function() {
+    expect('.temperature').toContainText(20);
+  });
+
+  it('shuold be 21 degrees when pressing the up button', function() {
+    $('.up').click();
+    expect('.temperature').toContainText(21);
+  });
+
+  it('shuold be 19 degrees when pressing the up button', function() {
+    $('.down').click();
+    expect('.temperature').toContainText(19);
+  });
+
+  it('shuold go back to 20 degrees when pressing the reset button', function() {
+    $('.down').click();
+    $('.reset').click();
+    expect('.temperature').toContainText(20);
+  });
+
+  it('should not go above 25 degrees when power saving mode is on', function() {
+    for(var i=0; i<6; i++) { $('.up').click(); };
+    expect('.temperature').toContainText(25);
+  });
+
+  it('should not go above 32 degrees when power saving mode is on', function() {
+    $('.power_save').trigger('click').trigger('change');
+    for(var i=0; i<13; i++) { $('.up').click(); };
+    expect('.temperature').toContainText(32);
+  });
+
+  it('should reset to 25 when turning power saving mode is turned on', function() {
+    $('.power_save').trigger('click').trigger('change');
+    for(var i=0; i<13; i++) { $('.up').click(); };
+    $('.power_save').trigger('click').trigger('change');
+    expect('.temperature').toContainText(25);
+  });
+
+  it('should not go below 10', function() {
+    for(var i=0; i<16; i++) { $('.down').click(); };
+    expect('.temperature').toContainText(10);
+  });
+
+  it('should display the temperature as yellow at 20 degrees', function() {
+    expect('.temperature').toHaveAttr('data-color', 'yellow');
+  });
+
+  it('should display the temperature as red at 25 degrees', function() {
+    for(var i=0; i<5; i++) { $('.up').click(); };
+    expect('.temperature').toHaveAttr('data-color', 'red');
+  });
+
+  it('should display the temperature as green below 18 degrees', function() {
+    for(var i=0; i<5; i++) { $('.down').click(); };
+    expect('.temperature').toHaveAttr('data-color', 'green');
+  });
+
+});
--- src/app_jquery.js
+++ src/app_jquery.js
@@ -3,18 +3,39 @@
 
 $( document ).ready(function() {
 
-  // $('.choosecity').click(function() {
-  //   $.ajax({
-  //     url: "api.openweathermap.org/data/2.5/find?q=London&units=metric",
-  //     dataType: 'xml'
-  //   }).done(function(data){
-  //     var weather = $('.weather');
-  //     weather.innerHTML = data;
-  //   }).fail(function(){
-  //     var weather = $('.weather');
-  //     weather.innerHTML = 'there was an error';
-  //   })
-  // })
+  ajax_call = function(city) { $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/find?q="+city+"&units=metric",
+      dataType: 'json',
+      success: function(data) {
+        $('.outside_temperature').html(Math.round(data.list[0].main.temp));
+      }
+    })
+  };
+
+  hide_input_elements = function() {
+    $(".desired_city").fadeOut(300);
+    $(".choose_city").fadeOut(300);
+  };
+
+  $('.choose_city').click(function() {
+    var city = $('.desired_city').val();
+    ajax_call(city);
+    $('.desired_city').val("");
+    hide_input_elements();
+  });
+
+  $(document).on("keyup", function(e) {
+    if(e.which == 13 && ($('.desired_city').is(":visible"))) {
+      // event.preventDefault();
+      var city = $('.desired_city').val();
+      ajax_call(city);
+      $('.desired_city').val("");
+      hide_input_elements();
+    } else if(e.which == 13) {
+      $(".desired_city").fadeIn(500);
+      $(".choose_city").fadeIn(500);
+    }
+  });
 
   thermostat = new Thermostat();
 
